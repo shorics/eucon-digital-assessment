c2f9bb942d4913b42da5b58f27adad9149152d73
shorics <noreplay@shorics.wtf>
2025-11-23 19:42:15 +0100

    replaced ormlite with hibernate

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ repositories {
 
 dependencies {
     implementation("javax.persistence:javax.persistence-api:2.2")
-    implementation("com.j256.ormlite:ormlite-jdbc:6.1")
+    implementation("org.hibernate.orm:hibernate-core:7.1.9.Final")
     implementation("com.opencsv:opencsv:5.12.0")
-    implementation("com.h2database:h2:2.4.240")
+    runtimeOnly("com.h2database:h2:2.4.240")
     testImplementation(kotlin("test"))
 }
 
